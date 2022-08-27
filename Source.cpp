5985d5e85027c739a829a431fc3845fe1b57c47b
NyiteX <kitheus09@gmail.com>
2022-08-27 19:14:32 +0300

    new menu!

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <string>
 #include <conio.h>
+#include <windows.h>
+#include <string>
+#define UP 72
+#define DOWN 80
+#define ESC 27
+#define ENTER 13
+
 using namespace std;
 
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+void GoToXY(short x, short y)
+{
+    SetConsoleCursorPosition(hStdOut, { x, y });
+}
+void ConsoleCursorVisible(bool show, short size)
+{
+    CONSOLE_CURSOR_INFO structCursorInfo;
+    GetConsoleCursorInfo(hStdOut, &structCursorInfo);
+    structCursorInfo.bVisible = show;
+    structCursorInfo.dwSize = size;
+    SetConsoleCursorInfo(hStdOut, &structCursorInfo);
+}
 bool Proverka(const string& b)
 {
     if (isalpha(b[0]) != 0)
@@ -12,6 +31,7 @@ bool Proverka(const string& b)
     else
         return 0;
 }
+
 class Vis
 {
     string wo;
@@ -168,7 +188,7 @@ public:
         {
             double time1 = clock();
             string b = "1";
-            bool l = false;       
+            bool l = false;
             while (b.size() > 1 || Proverka(b) == 0)
             {
                 cout << "Введите букву: ";
@@ -185,7 +205,7 @@ public:
                     l = true;
                 }
             }
-            if (l == true)
+            if (l == true)           
                 PrintStr(wo);
             else
             {
@@ -248,27 +268,63 @@ int main()
     srand(time(0));
     setlocale(LC_ALL, "ru");
     string path = "vis.txt";
-
     Vis A;
     A.LoadfromFile(path);
-    char vvod;
-    do
-    {
-        system("cls");
-        cout << "1. Игра.\nESC. Выход.\n";
-        vvod = _getch();
-        switch (vvod)
-        {
-        case'1':
-        {
-            A.PrintWords();
-            A.Play();
 
-            system("pause");
+    SetConsoleTitle(L"DA");
+    system("cls");
+    system("Color 0D");
+    ConsoleCursorVisible(false, 100);
+
+    string Menu[] = { "Игра", "Выход" };
+    int active_menu = 0;
+
+    char ch;
+    while (true)
+    {      
+        int x = 33, y = 17;
+        GoToXY(x, y);
+        
+        for (int i = 0; i < size(Menu); i++)
+        {
+            if (i == active_menu)
+                SetConsoleTextAttribute(hStdOut, 8);
+            else
+                SetConsoleTextAttribute(hStdOut, 7);
+            GoToXY(x, y++);
+            cout << Menu[i] << endl;
+        }
+        ch = _getch();
+        if (ch == -32) ch = _getch();
+        switch (ch)
+        {
+        case ESC:
+            exit(0);
+        case UP:
+            if (active_menu > 0)
+                --active_menu;
             break;
+        case DOWN:
+            if (active_menu < size(Menu) - 1)
+                ++active_menu;
+            break;
+        case ENTER:
+            switch (active_menu)
+            {
+            case 0:
+            {
+                system("cls");
+                GoToXY(x, y);
+                SetConsoleTextAttribute(hStdOut, 8);
+                A.PrintWords();
+                A.Play();
+                _getch();
+                system("cls");
+                break;
+            }
+            case 1:
+                exit(0);
+            }
         }
-        }
-    } while (vvod != 27);
-
-    return 0;
+    }
 }
